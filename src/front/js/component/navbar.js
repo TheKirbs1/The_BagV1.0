1f4b9a80df3dff444074c8f2c8b501734e1b7c4d
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-10-31 22:39:20 +0000

    massive navbar overhaul, need to fix styling and adjust join now

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,30 +1,99 @@
 import React, { useContext } from "react";
-import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
-import '../../styles/navbar.css'
+import { Context } from "../store/appContext";
+import basket from "../../img/DGBasket.png";
+
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
-	const { store, actions } = useContext(Context)
+  const { store, actions } = useContext(Context);
 
-	return (
-		<nav className="navbar navbar-secondary bg-secondary">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1"><img src="https://static.vecteezy.com/system/resources/previews/005/726/093/non_2x/house-icon-house-emoji-funny-house-icon-free-vector.jpg"></img></span>
-				</Link>
-				<div className="ml-auto">
-					{!store.token ?
-						<Link to="/login">
-							<button className="btn btn-primary">Login</button>
-						</Link>
-						:
-						<button 
-							className="btn btn-primary"
-							onClick= {() => actions.logout()}
-						>Log out</button>
-					}
-				</div>
-			</div>
-		</nav>
-	);
+  const handleLogout = () => {
+    actions.logout();
+    window.location.href = "/";
+  };
+
+  return (
+    <div>
+      <div className="join-now1 d-flex justify-content-center">
+        {!store.token ? (
+          <Link to="/signup">
+            <button className="btn join-now-btn"><b>Join Now!</b></button>
+          </Link>
+        ) : (
+          <div></div>
+        )}
+      </div>
+
+      <nav className="navbar">
+        <div className="nav-container container-fluid">
+          <Link to="/" id="home-logo">
+            <div className="logo-div">
+              <img
+                src={basket}
+                className="logo-img img-fluid circular-logo"
+                style={{ maxHeight: 300 }}
+                alt="Pet Logo/Home Button"
+              />
+            </div>
+          </Link>
+
+          <div className="menu">
+            <ul>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="40"
+                    height="40"
+                    fill="rgb(55, 58, 64)"
+                    className="bi bi-person-circle"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                    <path
+                      fillRule="evenodd"
+                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                    />
+                  </svg>
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li>
+                    {!store.token ? (
+                      <Link className="dropdown-item" to="/login">
+                        Login
+                      </Link>
+                    ) : (
+                      <Link className="dropdown-item" to="/private">
+                        Profile
+                      </Link>
+                    )}
+                  </li>
+                  <li><Link className="dropdown-item" to="/favorite">Favorites</Link></li>
+                  <li><Link className="dropdown-item" to="/account_settings">Account Settings</Link></li>
+                  <li><Link className="dropdown-item" to="/about_us">About us</Link></li>
+                  <li>
+                    {!store.token ? (
+                      <div></div>
+                    ) : (
+                      <button className="dropdown-item logout-btn" onClick={handleLogout}>
+                        Log out
+                      </button>
+                    )}
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
